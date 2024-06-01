4491cb00d262b177ade0a54c9ba764e589ac1b42
Vlad Solomenchuk <vlad@aramzamzam.net>
2024-06-01 14:30:41 -0700

    AudioBookFetcher 0.4

--- audiobookfetcher.rb
+++ audiobookfetcher.rb
@@ -1,14 +1,15 @@
 class Audiobookfetcher < Formula
-  desc "Retrieves audiobooks from akniga.org"
+  desc "Retrieves audio books from akniga.org"
   homepage "https://github.com/vovasty/AudioBookFetcher"
   url "https://github.com/vovasty/AudioBookFetcher.git",
-    tag:      "0.3",
-    revision: "d7ed36bd7014f63181a473a6ab6c529eff920faa"
+    tag:      "0.4",
+    revision: "d989555a900c09d2eb7abd452cc5b73461ae96ee"
   license "MIT"
-  version "0.3"
-  sha256 "e06d809e67846d9d33df11658b82adf53a1395ede3ad6e7588764b6b49e23d5b"
+  version "0.4"
+  sha256 "e06d809e67846d9d33df11658b82adf53a1395ede3ad6e7588764b6b49e23d5c"
 
-  depends_on xcode: ["12.0", :build]
+  depends_on xcode: ["15.0", :build]
+  depends_on "ffmpeg"
 
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
