b4d897090a60f0cc38f7bf83bef0b857954d1b58
Vladimir Solomenchuk <vlad@aramzamzam.net>
2026-02-08 21:49:46 -0800

    af 0.5.0

--- audiobookfetcher.rb
+++ audiobookfetcher.rb
@@ -2,11 +2,11 @@ class Audiobookfetcher < Formula
   desc "Retrieves audio books from akniga.org"
   homepage "https://github.com/vovasty/AudioBookFetcher"
   url "https://github.com/vovasty/AudioBookFetcher.git",
-    tag:      "0.4.7",
-    revision: "e6b8bab3ef0e5c29f6d7355148ccd3099845e4b9"
+    tag:      "0.5.0",
+    revision: "3691a48029239b872e6a8a4fde71a09f7ab40f10"
   license "MIT"
   version "0.4.7"
-  sha256 "a06d809e67846d9d33df12658b82adf53a1395ede3ad6e7588764b6b49e23d5e"
+  sha256 "a06d809e67846d9d33df12658b82adf53a1395ede3ad6e7588764b6b49e23d1e"
 
   depends_on xcode: ["15.0", :build]
   depends_on "ffmpeg"
